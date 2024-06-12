af790eaed5b203071c6434ef8f9ce2efe94e7bd5
casandrarey <1121294@apps.nsd.org>
2024-06-12 13:45:15 -0700

    added my files

---
+++ CRQuestion.java
@@ -0,0 +1,14 @@
+public class CRQuestion {
+
+    private String question;
+    private String answer;
+    private String optionA;
+    private String optionB;
+
+    public CRQuestion(String question, String answer, String optionA, String optionB){
+        this.question = question;
+        this.answer = answer;
+        this.optionA = optionA;
+        this.optionB = optionB;
+    }
+}
